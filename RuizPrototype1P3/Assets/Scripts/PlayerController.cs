a43a719479a798067140b8e43873e24b26127e75
MiaR730 <723420@student.dusd.net>
2024-02-29 11:41:08 -0800

    Add a pyrimid of bixes

--- RuizPrototype1P3/Assets/Scripts/PlayerController.cs
+++ RuizPrototype1P3/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forwrad 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forwrad based on vertical input 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horzontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);   
     }
 }
