92fdee97bd042b64ecef0908b21f66de35f6dca7
MiaR730 <723420@student.dusd.net>
2024-03-04 11:42:39 -0800

    Added icoming traffic and obsticles.

--- RuizPrototype1P3/Assets/Scripts/PlayerController.cs
+++ RuizPrototype1P3/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
-    private float horizontalInput;
-    private float forwardInput;
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
