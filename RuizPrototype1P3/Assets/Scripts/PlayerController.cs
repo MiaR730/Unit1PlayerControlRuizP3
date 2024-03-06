de5a8bd428f01269b3455f42a8b84c80cb0c2d21
MiaR730 <723420@student.dusd.net>
2024-03-06 11:43:02 -0800

    Added hood caamera and more obstcles. Finished Project.

--- RuizPrototype1P3/Assets/Scripts/PlayerController.cs
+++ RuizPrototype1P3/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,9 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed = 45.0f;
     public float horizontalInput;
     public float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,12 @@ public class PlayerController : MonoBehaviour
         // Moves the car forwrad based on vertical input 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horzontal input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);   
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
